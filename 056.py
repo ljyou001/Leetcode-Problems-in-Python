69767ae3f1f618c6dd0ac5f5e42fb898093cbbbe
Oliver <ljyou001@live.com>
2022-05-05 01:36:28 +0800

    comment

--- 056.py
+++ 056.py
@@ -4,7 +4,9 @@ class Solution:
             return intervals
         result = []
         intervals.sort(key=lambda x: x[0])
+        # attention here: lambda to do the sort
         for i in intervals: # !!!
+            # Below here, we directly operate result[-1], in case for multiple intervals overlap
             if len(result) == 0 or result[-1][1] < i[0]:
                 result.append(i)
             else:
