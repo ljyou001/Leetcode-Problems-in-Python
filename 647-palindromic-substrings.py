4badfa2d4d63e927c750e2b2a8135a60d107bf20
Oliver <ljyou001@live.com>
2022-05-23 12:12:35 +0800

    647 enum middle point

--- 647-palindromic-substrings.py
+++ 647-palindromic-substrings.py
@@ -1,16 +1,16 @@
-class Solution:
+class Solution_enum_middle_point:
     def countSubstrings(self, s: str) -> int:
         res = len(s)
         for i in range(len(s)):
             left, right = i - 1, i
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                res+=1
-                left-=1
-                right+=1
+                res += 1
+                left -= 1
+                right += 1
             left, right = i - 1, i + 1
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                res+=1
-                left-=1
-                right+=1
+                res += 1
+                left -= 1
+                right += 1
         return res
 
