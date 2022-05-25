b1d689e4d52fa51d7769183afe59118e264ef2f0
Oliver <ljyou001@live.com>
2022-05-25 21:21:46 +0800

    347 hashtable and sort

--- 347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements.py
@@ -1,13 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        res = {}
+        num_dict = {}
         for i in nums:
-            if i not in res.keys():
-                res[i] = 1
+            if i not in num_dict.keys():
+                num_dict[i] = 1
             else:
-                res[i] += 1
-        res = sorted(res.items(), key=lambda x: x[1], reverse=True)
+                num_dict[i] += 1
+        num_list = sorted(num_dict.items(), key = lambda x:x[1], reverse=True)
+        # this method will tansfer the dict to a tupled list
+        # dict.items() will return a dict items to tuples
+        print(num_list)
         result = []
         for i in range(k):
-            result.append(res[i][0])
+            result.append(num_list[i][0])
         return result
