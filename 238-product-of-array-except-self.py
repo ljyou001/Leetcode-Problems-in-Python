89dce1a5c0a68b84755cb1fe2bec89b4d23015a9
Oliver <ljyou001@live.com>
2022-05-27 00:19:13 +0800

    238 utilized memory

--- 238-product-of-array-except-self.py
+++ 238-product-of-array-except-self.py
@@ -44,3 +44,12 @@ class Solution_prefix_postfix_table:
 # Space: O(1)
 
 class Solution_util:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+        for i in range(1, len(nums)):
+            res[i] = res[i - 1] * nums[i - 1]
+        right = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= right
+            right *= nums[i]
+        return res
