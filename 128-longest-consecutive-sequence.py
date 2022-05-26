473923c2fb7c408f202d7209e53ba75cd503a9f6
Oliver <ljyou001@live.com>
2022-05-26 22:45:30 +0800

    128 set method

--- 128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence.py
@@ -14,7 +14,7 @@ class Solution_sort:
                 else: 
                     count = 1
         return max_count
-
+# sort method takes O(nlogn) time, O(1) space if input not counted in space
 
 class Solution_linear:
     def longestConsecutive(self, nums: List[int]) -> int:
@@ -33,4 +33,28 @@ class Solution_linear:
                 res[i] += 1
                 start += 1
         return max(res)
-            
+
+class Solution_set:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        res = 0
+        for i in nums:
+            number_plus = i + 1
+            number_minus = i - 1
+            # initialize two vars covers possible +1 and -1 in the set
+            count = 1
+            while number_plus in num_set:
+                # search for +1, +1, +1...
+                count += 1
+                num_set.remove(number_plus)
+                number_plus += 1
+            while number_minus in num_set:
+                # search for -1, -1, -1...
+                count += 1
+                num_set.remove(number_minus)
+                number_minus -= 1
+            # as long as they are continous, they should be counted into one same group
+            res = max(res, count)
+        return res
+        
+# Set method take O(n) time, O(n) space
