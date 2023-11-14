bab10a344e4ba6710fc092deda85a9cd956e749b
Oliver Lyu <ljyou001@live.com>
2023-11-13 19:58:42 -0800

    084 Monotonic Stack Method

--- 084-largest-rectangle-in-histogram.py
+++ 084-largest-rectangle-in-histogram.py
@@ -26,3 +26,23 @@ class Solution:
             res = max(res, area)
         
         return res
+    
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        res = 0
+        stack = [] #[[index, height],...]
+
+        for i, h in enumerate(heights):
+            start = i
+            while stack and h < stack[-1][1]:
+                index, height = stack.pop()
+                area = (i - index) * height
+                res = max(res, area)
+                start = index
+            stack.append([start, h])
+
+        for i, h in stack:
+            area = (len(heights) - i ) * h
+            res = max(res, area)
+        
+        return res
