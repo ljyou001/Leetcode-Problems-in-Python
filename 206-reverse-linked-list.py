b6773c0ffa80e4905161ae25668ea3f923d48ad1
Oliver Lyu <ljyou001@live.com>
2024-06-02 02:52:44 -0700

    206

--- 206-reverse-linked-list.py
+++ 206-reverse-linked-list.py
@@ -7,21 +7,15 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head == None: 
-            return None
-        if head.next == None:
-            return head
-        elem = []
-        while head != None:
-            elem.append(head.val)
-            head = head.next
-        res = ListNode()
-        cur = res
-        elem = elem[::-1]
-        for i in elem:
-            cur.next = ListNode(i) 
-            cur = cur.next
-        return res.next
+        prev = None
+        cur = head
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+        return prev
+
 
 class Solution_iter:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
