b2a4e379caa8ddb18fcf1a94ace794c11c28857f
Oliver Lyu <ljyou001@live.com>
2024-06-04 17:33:28 -0700

    002

--- 002-add-two-numbers..py
+++ 002-add-two-numbers..py
@@ -46,3 +46,24 @@ class Solution_Github:
         if addnext != 0:
             cur.next = ListNode(addnext)
         return res.next
+    
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
+        borrow = 0
+        while l1 or l2 or borrow:
+            sum_val = 0
+            if l1:
+                sum_val += l1.val
+                l1 = l1.next
+            if l2:
+                sum_val += l2.val
+                l2 = l2.next
+            if borrow:
+                sum_val += borrow
+            value = sum_val % 10
+            borrow = sum_val // 10
+            cur.next = ListNode(val=value)
+            cur = cur.next
+        return dummy.next
