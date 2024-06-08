f1fc0d5da8cf302795b50cc199168440e089abd4
Oliver <ljyou001@live.com>
2024-06-07 17:11:56 -0700

    136 set and Counter method

--- 136-single-number.py
+++ 136-single-number.py
@@ -4,3 +4,25 @@ class Solution:
         for num in nums:
             xor ^= num
         return xor
+
+class Solution1:
+    def singleNumber(self, nums: List[int]) -> int:
+        num_set = set()
+        for num in nums:
+            if num in num_set:
+                num_set.remove(num)
+            else:
+                num_set.add(num)
+        return list(num_set)[0]
+
+class Solution2:
+    """
+    @param a: An integer array
+    @return: An integer
+    """
+    def single_number(self, a: List[int]) -> int:
+        from collections import Counter
+        num_list = Counter(a)
+        for key, val in num_list.items():
+            if val == 1:
+                return key
