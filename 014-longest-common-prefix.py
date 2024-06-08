67a8875b82b136cd6e796d83fe599b76018e362f
Oliver <ljyou001@live.com>
2024-06-07 18:50:12 -0700

    014

--- 014-longest-common-prefix.py
+++ 014-longest-common-prefix.py
@@ -9,3 +9,18 @@ class Solution:
                 if i == len(j) or j[i] != chara:
                     return strs[0][0:i]
         return strs[0]
+
+class Solution2:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs or not strs[0]:
+            return ""
+        res = ""
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for j in range(len(strs)):
+                if i >= len(strs[j]):
+                    return res
+                if char != strs[j][i]:
+                    return res
+            res += char
+        return res
