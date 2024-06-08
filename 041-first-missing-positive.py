a05cadfb5042e3c47de4cb97b401495748e337af
Oliver <ljyou001@live.com>
2024-06-07 21:15:02 -0700

    041

--- 041-first-missing-positive.py
+++ 041-first-missing-positive.py
@@ -4,3 +4,16 @@ class Solution:
         for i in range(1, len(nums) + 2):
             if i not in nums:
                 return i
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        if not nums:
+            return 1
+        max_num = max(nums)
+        if max_num <= 0:
+            return 1
+        nums = set(nums)
+        for i in range(1, max_num):
+            if i not in nums:
+                return i
+        return max_num + 1 
