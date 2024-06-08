e6dce2be397567d5b061e35633a8cc5a6d85aa37
Oliver <ljyou001@live.com>
2024-06-07 20:16:10 -0700

    049

--- 049-group-anagrams.py
+++ 049-group-anagrams.py
@@ -1,4 +1,4 @@
-class Solution:
+class SolutionSort:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         if len(strs) <= 1:
             return [strs]
@@ -12,3 +12,21 @@ class Solution:
                 res[ind].append(i)
                 
         return res.values()
+
+class SolutionCounter:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = {}
+        for item in strs:
+            item_count = tuple(self.count_string(item))
+            if res.get(item_count):
+                res[item_count].append(item)
+            else:
+                res[item_count] = [item]
+        return res.values()
+        
+    def count_string(self, string):
+        LENGTH_ALPHABET = 26
+        count = [0 for _ in range(LENGTH_ALPHABET)]
+        for char in string:
+            count[ord(char) - ord('a')] += 1
+        return count
