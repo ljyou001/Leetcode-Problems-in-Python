22149ce22e49216b453d2d4e984c2274659cc01c
Oliver Lyu <ljyou001@live.com>
2024-06-08 01:36:00 -0700

    283

--- 283-move-zeroes.py
+++ 283-move-zeroes.py
@@ -16,3 +16,15 @@ class Solution:
                     k += 1
             else:
                 i += 1
+
+class Solution2:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            
