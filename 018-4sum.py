5f701ec1b2f5a784dae9814c005b2fb540bbef04
Oliver <ljyou001@live.com>
2024-06-16 00:21:10 -0700

    018

--- 018-4sum.py
+++ 018-4sum.py
@@ -34,3 +34,46 @@ class Solution:
                 elif threeSum > target:
                     right -= 1
         return res
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        if len(nums) < 4:
+            return []
+        res = []
+        nums.sort()
+        for i in range(len(nums) - 3):
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
+            for j in range(i + 1, len(nums) - 2):
+                if j > i + 1 and nums[j - 1] == nums[j]:
+                    continue
+                result = self.two_pointer_search(
+                    target,
+                    nums[i],
+                    nums[j],
+                    nums[j + 1: len(nums)]
+                )
+                res += result
+
+        return res
+    
+    def two_pointer_search(self, target, i, j, array):
+        target = target - i - j
+        left = 0
+        right = len(array) - 1
+        result = []
+        while left < right:
+            two_sum = array[left] + array[right]
+            if two_sum < target:
+                left += 1
+            elif two_sum > target:
+                right -= 1
+            else: # ==
+                result.append([i, j, array[left], array[right]])
+                while left < right and array[left] == array[left + 1]:
+                    left += 1
+                while left < right and array[right] == array[right - 1]:
+                    right -= 1
+                left += 1
+                right -= 1
+        return result
