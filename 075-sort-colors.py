f11ca1ed53b0e716b1655fd8e24a80d72213809e
Oliver <ljyou001@live.com>
2024-06-17 01:47:32 -0700

    075 partition method

--- 075-sort-colors.py
+++ 075-sort-colors.py
@@ -8,3 +8,24 @@ class Solution:
                 while nums[i-1] > nums[i] and i > 0:
                     nums[i-1], nums[i] = nums[i], nums[i-1]
                     i -= 1
+
+class SolutionPartition:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        self.partition(nums, 1)
+        self.partition(nums, 2)
+
+    def partition(self, nums, pivot):
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            while left <= right and nums[left] < pivot:
+                left += 1
+            while left <= right and nums[right] >= pivot:
+                right -= 1
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
