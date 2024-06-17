09bdd4e5d4ac1bb855e2d4ac3b9696bfca3118f9
Oliver <ljyou001@live.com>
2024-06-16 17:36:59 -0700

    905 partition method

--- 905-sort-array-by-parity.py
+++ 905-sort-array-by-parity.py
@@ -12,3 +12,19 @@ class Solution_me:
 class Solution_Github:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         return sorted(nums, key=lambda x: x % 2)
+
+class SolutionPartition:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            while left <= right and nums[left] % 2 == 0:
+                left += 1
+            while left <= right and nums[right] % 2 != 0:
+                right -= 1
+            
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        return nums
