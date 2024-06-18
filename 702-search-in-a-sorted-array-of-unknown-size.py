0e7bb9003b28eb9b39a06eb8c06b38cc743c72bd
Oliver <ljyou001@live.com>
2024-06-18 02:00:12 -0700

    702 Exponential Backoff + Binary Search

---
+++ 702-search-in-a-sorted-array-of-unknown-size.py
@@ -0,0 +1,26 @@
+class Solution:
+    """
+    @param reader: An instance of ArrayReader.
+    @param target: An integer
+    @return: An integer which is the first index of target.
+    """
+    # 702. Search in a Sorted Array of Unknown Size
+    def searchBigSortedArray(self, reader, target):
+        right = 0
+        while reader.get(right) < target:
+            right = right * 2 + 1
+        if reader.get(right) == target:
+            return right
+        
+        left = right // 2
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if reader.get(mid) < target:
+                left = mid
+            else:
+                right = mid
+        if reader.get(left) == target:
+            return left
+        if reader.get(right) == target:
+            return right
+        return -1
