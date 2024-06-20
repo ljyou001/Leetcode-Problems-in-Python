7a130dfb5bc2552e3b28d1ca7e032111fd0097eb
Oliver <ljyou001@live.com>
2024-06-20 13:05:43 -0700

    069 binary search template

--- 069-sqrtx.py
+++ 069-sqrtx.py
@@ -10,3 +10,17 @@ class Solution:
             else:
                 r = mid - 1
         return ans
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right, ans = 0, x, -1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if mid * mid <= x:
+                ans = mid
+                left = mid
+            else:
+                right = mid
+        if right * right <= x:
+            ans = right
+        return ans
