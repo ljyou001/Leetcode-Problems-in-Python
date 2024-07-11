bca3f130e25664878642dd2f61e43d4d7ccf2d58
Oliver <ljyou001@live.com>
2024-07-11 02:05:18 -0700

    017 Letter Combinations of a Phone Number: DFS template for mapping structure

--- 017-letter-combinations-of-a-phone-number.py
+++ 017-letter-combinations-of-a-phone-number.py
@@ -10,3 +10,32 @@ class Solution:
                     tmp.append(j + k)
             res = tmp
         return res
+
+
+MAPPING = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+}
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        res = []
+        self.dfs(digits, 0, [], res)
+        return res
+
+    def dfs(self, digits, index, path, res):
+        if len(path) == len(digits):
+            return res.append(''.join(path))
+        for char in MAPPING[digits[index]]:
+            path.append(char)
+            self.dfs(digits, index + 1, path, res)
+            path.pop()
+
