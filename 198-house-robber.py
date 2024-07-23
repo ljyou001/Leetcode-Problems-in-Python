6b336c645cf80c5c9bf43586c2fb08c80bdf90a3
Oliver <ljyou001@live.com>
2024-07-23 16:17:42 -0700

    198 house-robber: Dynamic Programming, DFS Memorization Search, Recursion

--- 198-house-robber.py
+++ 198-house-robber.py
@@ -1,4 +1,4 @@
-class Solution:
+class SolutionIncursionDP:
     def rob(self, nums: List[int]) -> int:
         n1 = 0
         n2 = nums[0]
@@ -12,3 +12,24 @@ class Solution:
             n1 = old_n2
 
         return n2
+
+class SolutionDFSMemorization:
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+        return max(self.dfs(nums, 0, memo), self.dfs(nums, 1, memo))
+
+    def dfs(self, nums, index, memo):
+        if index in memo:
+            return memo[index]
+        if index >= len(nums):
+            return 0
+        
+        value = 0
+        for i in range(index + 2, len(nums)):
+            value = max(
+                value,
+                self.dfs(nums, i, memo)
+            )
+        
+        memo[index] = value + nums[index]
+        return memo[index]
