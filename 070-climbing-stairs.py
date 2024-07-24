0bd4efae18125417db32feb8a61e67e88ff244ee
Oliver <ljyou001@live.com>
2024-07-23 17:51:55 -0700

    070 climbing-stairs: dynamic programming, for loop, circular array to utilize memory

--- 070-climbing-stairs.py
+++ 070-climbing-stairs.py
@@ -41,4 +41,30 @@ class SolutionMemorizationSearch:
 
         return self.memo[n]
 
+class SolutionReversedForLoop:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[n] = 1
+        if n - 1 > -1: 
+            dp[n - 1] = 1
+        for i in range(n - 2, -1, -1):
+            dp[i] = dp[i + 1] + dp[i + 2]
+        return dp[0]
 
+class SolutionForLoop:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+
+class SolutionCircularArray:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * 3
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i % 3] = dp[(i - 1) % 3] + dp[(i - 2) % 3]
+        return dp[n % 3]
