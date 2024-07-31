b9b486964c825f7a8dc4a3cc69137decafc1fb38
Oliver <ljyou001@live.com>
2024-07-30 18:16:03 -0700

    300 longest-increasing-subsequence: 1D Dynamic programming, iterative, DP storage

---
+++ 300-longest-increasing-subsequence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1] * (len(nums) + 1)
+        for i in range(len(nums)):
+            for j in range(i, -1, -1):
+                if nums[i] > nums[j] and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+        return max(dp)
