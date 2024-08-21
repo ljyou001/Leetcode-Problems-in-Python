d335ce4b284aeda1db63c73f47701a618bf782fb
Oliver <ljyou001@live.com>
2024-08-21 01:54:03 -0700

    297 serialize-and-deserialize-binary-tree: Binary Tree, post-order traversal, recursive DFS, serialize and deserialize

--- 297-serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree.py
@@ -7,16 +7,13 @@
 
 class CodecPreorderTraversal:
     def serialize(self, root):
-        """Encodes a tree to a single string.
-        
-        :type root: TreeNode
-        :rtype: str
-        """
         if not root:
             return ''
         res = []
         self._serialize(root, res)
         return ','.join(res)
+        # [1,2,3,null,null,4,5]
+        # to ['1', '2', 'N', 'N', '3', '4', 'N', 'N', '5', 'N', 'N']
 
     def _serialize(self, root, res):
         if not root:
@@ -26,13 +23,7 @@ class CodecPreorderTraversal:
         self._serialize(root.left, res)
         self._serialize(root.right, res)
 
-
     def deserialize(self, data):
-        """Decodes your encoded data to tree.
-        
-        :type data: str
-        :rtype: TreeNode
-        """
         if not data:
             return None
         data = deque(data.split(','))
@@ -49,7 +40,39 @@ class CodecPreorderTraversal:
             node.right = self._deserialize(data)
         return node
 
-# Your Codec object will be instantiated and called as such:
-# ser = Codec()
-# deser = Codec()
-# ans = deser.deserialize(ser.serialize(root))
+class CodecPostorderTraversal:
+    def serialize(self, root):
+        if not root:
+            return ''
+        res = []
+        self._serialize(root, res)
+        return ','.join(res)
+        # [1,2,3,null,null,4,5]
+        # to ['N', 'N', '2', 'N', 'N', '4', 'N', 'N', '5', '3', '1']
+
+    def _serialize(self, root, res):
+        if not root:
+            return res.append('N')
+        
+        self._serialize(root.left, res)
+        self._serialize(root.right, res)
+        res.append(str(root.val))
+
+    def deserialize(self, data):
+        if not data:
+            return None
+        data = data.split(',')
+        return self._deserialize(data)
+
+    def _deserialize(self, data):
+        if not data:
+            return None
+
+        thisval = data.pop()
+        node = TreeNode(int(thisval)) if thisval != 'N' else None
+        if node:
+            # 根据上面的输出可以看到后续遍历就是从右到左，基本跟前序遍历差不多
+            node.right = self._deserialize(data)
+            node.left = self._deserialize(data)
+        return node
+
