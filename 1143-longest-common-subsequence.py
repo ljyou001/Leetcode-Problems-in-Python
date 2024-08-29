097e3d57d570b54264f485c9aa54aa0df40ed862
Oliver <ljyou001@live.com>
2024-08-29 15:12:44 -0700

    1143 longest-common-subsequence: 2D dynamic programming, iterative method

--- 1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence.py
@@ -18,3 +18,25 @@ class SolutionMemoSearch:
                 self.dfs(text1, text2, i1 + 1, i2, memo),
             )
         return memo[(i1, i2)]
+
+class SolutionDP:
+    # https://onlineboard.eu/b/UfbLy6EW
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+
+        for i in range(1, len(text1) + 1):
+            for j in range(1, len(text2) + 1):
+                if text2[j - 1] == text1[i - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
+        return dp[len(text1)][len(text2)]
+
+        # WHY CANNOT: ?
+        
+        # for i in range(1, len(text1) + 1):
+        #     for j in range(1, len(text2) + 1):
+        #         dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+        #         if text2[j - 1] == text1[i - 1]:
+        #             dp[i][j] += 1
