f1f80828297487260dacaebab0506d1533d5f497
Oliver <ljyou001@live.com>
2024-09-08 22:41:18 -0700

    1123 lowest-common-ancestor-of-deepest-leaves: Binary Tree, Recursive divide and conqur, bottom up height, META real

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        return self.dfs(root)[0]
+
+    def dfs(self, root):
+        if not root.left and not root.right:
+            return (root, 1)
+        
+        left_root, left_height = (root, 0)
+        right_root, right_height = (root, 0)
+
+        if root.left:
+            left_root, left_height = self.dfs(root.left)
+        if root.right:
+            right_root, right_height = self.dfs(root.right)
+
+        if left_height > right_height:
+            return (left_root, left_height + 1)
+        elif left_height < right_height:
+            return (right_root, right_height + 1)
+        else:
+            return (root, left_height + 1)
