c350c99380e5fde9dcac602a72bd22f21be868f0
Oliver <ljyou001@live.com>
2024-09-13 13:10:40 -0700

    056 merge-intervals: list operation, key sort

--- 056-merge-intervals.py
+++ 056-merge-intervals.py
@@ -1,5 +1,23 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals = sorted(intervals, key=lambda x: x[0])
+
+        res = []
+        interval = [intervals[0][0], intervals[0][1]]
+        for i in range(len(intervals)):
+            start, end = intervals[i][0], intervals[i][1]
+            if start > interval[1]:
+                res.append(interval[:])
+                interval = [start, end]
+            else:
+                interval[1] = max(interval[1], end)
+        
+        res.append(interval[:])
+        return res
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if len(intervals) < 2:
             return intervals
         result = []
