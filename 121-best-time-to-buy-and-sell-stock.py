e0aecf63f0d72c1e9bcb37828d2266e28606413d
Oliver <ljyou001@live.com>
2024-09-12 19:05:34 -0700

    121 best-time-to-buy-and-sell-stockL 1D Dynamic programming, iterative loop

--- 121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock.py
@@ -11,3 +11,26 @@ class Solution:
                 i = j
             j += 1
         return res
+
+class Status:
+    HOLD = 1
+    CLEAR = 0
+
+class SolutionDP:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = [[0] * (2) for _ in range(len(prices) + 1)]
+        dp[0][Status.CLEAR] = 0
+        dp[0][Status.HOLD] = -prices[0]
+        res = 0
+
+        for i in range(1, len(prices) + 1):
+            dp[i][Status.CLEAR] = max(
+                dp[i - 1][Status.CLEAR],
+                dp[i - 1][Status.HOLD] + prices[i - 1],
+            )
+            dp[i][Status.HOLD] = max(
+                dp[i - 1][Status.HOLD],
+                -prices[i - 1]
+            )
+        
+        return dp[len(prices)][Status.CLEAR]
