3083f57c7859ab7375d3d7e926a9612c31625a82
Oliver <ljyou001@live.com>
2024-09-21 00:23:48 -0700

    lint011 Search-Range-in-Binary-Search-Tree: Binary search tree, inorder traversal, recursive DFS

---
+++ lint011-Search-Range-in-Binary-Search-Tree.py
@@ -0,0 +1,33 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: param root: The root of the binary search tree
+    @param k1: An integer
+    @param k2: An integer
+    @return: return: Return all keys that k1<=key<=k2 in ascending order
+    """
+    def search_range(self, root: TreeNode, k1: int, k2: int) -> List[int]:
+        # write your code here
+        if not root:
+            return []
+        res = []
+        self.dfs(root, k1, k2, res)
+        return res
+
+    def dfs(self, root, k1, k2, res):
+        if not root:
+            return
+        
+        if root.val > k1:
+            self.dfs(root.left, k1, k2, res)
+        if k1 <= root.val <= k2:
+            res.append(root.val)
+        if root.val < k2:
+            self.dfs(root.right, k1, k2, res)
