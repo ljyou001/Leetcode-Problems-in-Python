0920601924d81a3fa085b9be7f3da590ca8801b9
Oliver <ljyou001@live.com>
2024-11-04 23:59:27 +0800

    981 time-based-key-value-store: Right Approach Binary search, hash table & list operation

--- 981-time-based-key-value-store.py
+++ 981-time-based-key-value-store.py
@@ -1,3 +1,40 @@
+class TimeMapTemplate:
+
+    def __init__(self):
+        self.kv = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.kv:
+            self.kv[key].append((timestamp, value))
+        else:
+            self.kv[key] = [(timestamp, value)]
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.kv:
+            return ''
+        
+        val_list = self.kv[key]
+        left = 0
+        right = len(val_list) - 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if val_list[mid][0] > timestamp:
+                right = mid
+            else:
+                left = mid
+        
+        if val_list[left][0] > timestamp:
+            return ''
+        if val_list[right][0] <= timestamp:
+            return val_list[right][1]
+        return val_list[left][1]
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
+
+
 class TimeMap:
 
     def __init__(self):
